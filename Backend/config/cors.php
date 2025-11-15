f76d8c4360095b3c1c53bebffcd8f2b89a2b7c31
Ji-ru <164502698+Ji-ru@users.noreply.github.com>
2025-11-16 07:33:09 +0800

    Modified cors.php

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -7,7 +7,8 @@ return [
         'http://localhost:5173',
         'http://localhost:4173',
         'http://localhost:8000',
-        'http://127.0.0.1:8000'
+        'http://127.0.0.1:8000',
+        'https://oceanify-official-page.vercel.app'
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
